60ccf2c9ce954fe0281b1a07b9d9ad3c1df60600
Bidhampola <brianemperor14@gmail.com>
2021-07-24 14:00:49 +0300

    adding password generator

---
+++ password_generator.py
@@ -0,0 +1,28 @@
+### week 1 project... password generator
+
+import string
+import random
+
+print("Welcome to password generator! ")
+
+#input the length of the password
+length=int(input('\n Enter the length of the password: '))
+
+#defining data
+num=string.digits
+lower=string.ascii_lowercase
+upper=string.ascii_uppercase
+symbols=string.punctuation
+
+#combining all data
+all = upper + lower + num + symbols
+
+#using random
+
+temp=random.sample(all,length)
+
+#creating a password
+password = "".join(temp)
+
+# output password
+print(password)
